5ba92bfe738602342d818b5bffcaa71e432dc700
florentin-a-p <florentin.purnama@gmail.com>
2019-12-22 20:18:27 +0700

    resolved merge conflict ehehe

--- random_script.py
+++ random_script.py
@@ -4,6 +4,7 @@ import numpy as np
 print("this is some random python script")
 print("another random comment")
 
+
 some_var1 = 3
 some_var2 = 4
 
@@ -11,3 +12,9 @@ some_sum_var = some_var1 + some_var2
 some_sub_var = some_var1 - some_var2
 
 print("I messed up your code!")
+
+some_str1 = 'aku '
+some_str2 = 'adalah '
+
+some_sum_str = some_str1 + some_str2
+
